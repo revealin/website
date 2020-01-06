92e8d116b20e13c2c2b8fe84ebaaa4eb81288e65
AlexisCapdepon <capdepon.alexis@gmail.com>
2020-01-06 21:40:44 +0100

    ded

--- src/components/UserAdmin.vue
+++ src/components/UserAdmin.vue
@@ -1,42 +1,12 @@
 <template>
+   <div>
+       <h2>Liste d'utilisateurs</h2>
     <v-data-table
             :headers="headers"
             :items="users"
             sort-by="calories"
             class="elevation-1"
     >
-        <template v-slot:top>
-            <v-toolbar flat color="white">
-                <v-dialog v-model="dialog" max-width="500px">
-                    <v-card>
-                        <v-card-text>
-                            <v-container>
-                                <v-row>
-                                    <v-col cols="12" sm="6" md="4">
-                                        <v-text-field v-model="editedItem.name" label="Nom"/>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                        <v-text-field v-model="editedItem.email" label="Email"/>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                        <v-text-field v-model="editedItem.gender" label="Genre"/>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                        <v-text-field v-model="editedItem.createdAt" label="Crée"/>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                        <v-text-field v-model="editedItem.birth" label="Date de naissance"/>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                        <v-text-field v-model="editedItem.description" label="Description"/>
-                                    </v-col>
-                                </v-row>
-                            </v-container>
-                        </v-card-text>
-                    </v-card>
-                </v-dialog>
-            </v-toolbar>
-        </template>
         <template v-slot:item.action="{ item }">
             <v-icon
                 small
@@ -46,6 +16,7 @@
             </v-icon>
         </template>
     </v-data-table>
+   </div>
 </template>
 <script>
     import { mapState } from 'vuex';
