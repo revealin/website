78891d12ac29243c1d061013efb303e7dedd6be2
AlexisCapdepon <capdepon.alexis@gmail.com>
2020-01-09 09:29:41 +0100

    banned/ 404

--- src/components/ReportAdmin.vue
+++ src/components/ReportAdmin.vue
@@ -1,6 +1,5 @@
 <template>
   <v-container fluid>
-    {{reportedUser}}
     <v-data-iterator
         :items="items"
         item-key="name"
@@ -76,7 +75,7 @@
             expand: true,
             items: [
                 {
-                    name: 'Frozen Yogurt',
+                    name: 'Bertrand',
                     calories: 159,
                     fat: 6.0,
                     carbs: 24,
@@ -86,7 +85,7 @@
                     iron: '1%',
                 },
                 {
-                    name: 'Ice cream sandwich',
+                    name: 'Jack',
                     calories: 237,
                     fat: 9.0,
                     carbs: 37,
@@ -94,26 +93,6 @@
                     sodium: 129,
                     calcium: '8%',
                     iron: '1%',
-                },
-                {
-                    name: 'Eclair',
-                    calories: 262,
-                    fat: 16.0,
-                    carbs: 23,
-                    protein: 6.0,
-                    sodium: 337,
-                    calcium: '6%',
-                    iron: '7%',
-                },
-                {
-                    name: 'Cupcake',
-                    calories: 305,
-                    fat: 3.7,
-                    carbs: 67,
-                    protein: 4.3,
-                    sodium: 413,
-                    calcium: '3%',
-                    iron: '8%',
                 },
             ],
         }),
